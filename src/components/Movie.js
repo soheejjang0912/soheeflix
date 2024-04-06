95c73218ad49e1d083c2357e13a296dda41b9f70
soheejjang <soheejjagn0912@gmail.com>
2024-04-06 23:57:48 +0900

    Router
    Home.js 에서 Movie 경로 import 때문에 App.js 에서 Cannot find module 에러 발생함

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({coverImage, title, summary, genres}){
     return (
         <div>
             <img src={coverImage} alt={title}></img>
-            <h2>{title}</h2>
+            <h2>
+                <Link to="/movie">{title}</Link>
+            </h2>
+
             <p>{summary}</p>
             <ul>
             {genres.map((g) => (
---
+++ src/routes/Home.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie"; //이거때문에 진짜 시간 엄청 씀..
+
+function Home(){
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    /* then으로 비동기 코드 가져오기 
+    useEffect(()=>{
+        fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`) //문자열 안에 변수(variable) 넣을 때 `` 사용 
+        .then((response) => response.json())
+        .then(json => {
+            setMovies(json.data.movies);
+            setLoading(false);}
+        ); 
+    },[]);*/
+    const getMovies = async() => {
+        const response = await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+        );
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
+    useEffect(()=> {
+        getMovies();
+    }, []);
+    console.log(movies);
+    return (
+        <div>
+        {
+            loading ? <h1>loading...</h1> 
+            : (<div>
+            {
+                movies.map((movie)=>
+                <Movie 
+                    key={movie.id}
+                    coverImage={movie.medium_cover_image}
+                    title={movie.title}
+                    summary={movie.summary}
+                    genres={movie.genres}
+                />
+                ) 
+            }
+            </div>)
+        }
+        <h1>soheeflix</h1>
+        </div>
+    );
+}
+export default Home;
