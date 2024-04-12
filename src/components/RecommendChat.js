d624e3846b3ff97c48d2185dee79c5c628ec480b
soheejjang <soheejjagn0912@gmail.com>
2024-04-13 02:57:55 +0900

    recommend 페이지 개발중
    전송 메시지와 추천 메시지의 위치 처리

--- src/components/RecommendChat.js
+++ src/components/RecommendChat.js
@@ -1,8 +1,11 @@
+import styles from "../css/Recommend.module.css";
 
 function RecommendChat({id, coverImage, title, index}){
-    return (
-        <div> 
-            
+    return ( 
+        <div>  
+            <div>
+                <img src={coverImage} alt={title}></img>
+            </div>
         </div> 
     );
 }
--- src/routes/Recommend.js
+++ src/routes/Recommend.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import RecommendChat from "../components/RecommendChat";
 import styles from "../css/Recommend.module.css";
 import sohee from "../img/sohee.jpg";
 
 function Recommend(){
+    const [messageContent, setMessageContent] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [recommendMovies, setRecommendMovies] = useState([]);
+   
+    // 메시지 입력값 변경 시 호출되는 함수
+    const handleMessageContentChange = (event) => {
+        setMessageContent(event.target.value);
+    };
+
+    // 전송 버튼 클릭 시 호출되는 함수
+    const sendButtonClick = () => {
+        // 입력한 메시지를 메시지 목록에 추가하고, 메시지 내용 초기화
+        setMessages([...messages, messageContent]);
+        setMessageContent("");
+
+        recommendMoviesApi();
+        
+    };
+ 
+    const recommendMoviesApi = async () => {
+        // 추천 영화 목록 가져오기
+        const response = await fetch(
+            `https://yts-proxy.now.sh/list_movies.json?genre=${messageContent}`
+        );
+        const json = await response.json();
+        setRecommendMovies(prevMovies => [...prevMovies, ...json.data.movies]); 
+    }; 
     
     return ( 
         <div>
@@ -17,18 +45,45 @@ function Recommend(){
                         <div className={styles.sohee}>
                             <p>안녕하세요! 영화 추천을 도와드리겠습니다. 어떤 장르의 영화를 좋아하시나요?</p>
                         </div> 
+                    </div> 
+                    <div>
+                        {/* 메시지 목록 렌더링 */}
+                        {messages.map((message, index) => (
+                            <div key={index} className={styles.message__container}>
+                                <div className={styles.message}>
+                                    <p>{message}</p>
+                                </div>
+                            </div>
+                        ))}
+                        {/* 추천 영화 목록 렌더링 */}
+                        {recommendMovies.map((movie, index) => (
+                            <div key={index} className={styles.message__container}>
+                                <div>
+                                    <img src={movie.medium_cover_image} alt={movie.title}></img>
+                                </div>
+                            </div> 
+                            /*<RecommendChat 
+                                key={movie.id}
+                                id={movie.id}
+                                coverImage={movie.medium_cover_image}
+                                title={movie.title}
+                                index={movie.index} 
+                            />*/
+                        ))}
                     </div>
-
-                    <div className={styles.message__container}>  
-                        <div className={styles.message}>
-                            <p>안녕하세요! 영화 추천을 도와드리겠습니다. 어떤 장르의 영화를 좋아하시나요?</p>
-                        </div> 
-                    </div>
+                    
+                    
                 </div>
                 <div className={styles.chat__send}>
                     <div className={styles.input__container}>
-                        <input type="text" className={styles.input__field} placeholder="메시지를 입력하세요..." />
-                        <button className={styles.input__button}>전송</button>
+                        <input 
+                            type="text" 
+                            className={styles.input__field} 
+                            placeholder="메시지를 입력하세요..." 
+                            value={messageContent}
+                            onChange={handleMessageContentChange} 
+                        />
+                        <button onClick={sendButtonClick} className={styles.input__button}>전송</button>
                     </div>
                      
                 </div>
