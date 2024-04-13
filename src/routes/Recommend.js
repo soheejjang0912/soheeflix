ff1b78dc2e77d7782d806912ac482bf230d32ace
soheejjang <soheejjagn0912@gmail.com>
2024-04-13 13:11:51 +0900

    recommend update시 스크롤 최 하단 이동

--- src/routes/Recommend.js
+++ src/routes/Recommend.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
 import RecommendChat from "../components/RecommendChat";
 import styles from "../css/Recommend.module.css";
@@ -6,7 +6,8 @@ import sohee from "../img/sohee.jpg";
 
 function Recommend(){
     const [message, setMessage] = useState('');
-    const [recommend, setRecommen] = useState([]);
+    const [recommend, setRecommend] = useState([]);
+    const scrollRef = useRef(null); 
 
     const handleMessageChange = (event) => {
         setMessage(event.target.value);
@@ -15,20 +16,24 @@ function Recommend(){
     const handleSendMessage = () => {
         if (message.trim() !== '') { 
             // 메시지 전송 후 추가적인 처리: 메시지를 입력한 사용자에 대한 메시지를 추가
-            setRecommen(recommendContent => [
+            setRecommend(recommendContent => [
                 ...recommendContent,
                 { message, type: 'user' }, // 사용자가 입력한 메시지
                 { message: `${message}을 입력하셨습니다`, type: 'sohee' } //api
             ]);
             setMessage('');
         }
-    };
+    }; 
 
+    // 채팅 메시지가 업데이트될 때마다 스크롤을 아래로 이동
+    useEffect(() => { 
+        scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }, [recommend]); 
     return (
         <div>
             <Header />
             <div className={styles.chat}>
-                <div className={styles.chat__message}>
+                <div className={styles.chat__message}  ref={scrollRef}>
                     <div className={styles.message__container}> 
                         <div className={styles.image__container}>
                             <img className={styles.size} src={sohee} alt="이미지" />
